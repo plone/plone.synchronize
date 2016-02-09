3fd8c198a10b9079c22cbbe33355f65a7dee5eee
Maurits van Rees <maurits@vanrees.org>
2016-02-09 21:13:11 +0100

    Added classifiers in setup.py.
    [ci skip]

--- setup.py
+++ setup.py
@@ -2,29 +2,33 @@ from setuptools import setup, find_packages
 
 version = '1.0.2.dev0'
 
-setup(name='plone.synchronize',
-      version=version,
-      description="Simple decorators to support synchronized methods",
-      long_description=open("README.rst").read() + "\n" +
-                       open("CHANGES.rst").read(),
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
+setup(
+    name='plone.synchronize',
+    version=version,
+    description="Simple decorators to support synchronized methods",
+    long_description=(open("README.rst").read() + "\n" +
+                      open("CHANGES.rst").read()),
+    # Get more strings from
+    # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "License :: OSI Approved :: BSD License",
-        ],
-      keywords='synchronized lock decorator',
-      author='Martin Aspeli',
-      author_email='optilude@gmail.com',
-      url='http://pypi.python.org/pypi/plone.synchronize',
-      license='BSD',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['plone'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-      ],
-      entry_points="""
-      """,
-      )
+    ],
+    keywords='synchronized lock decorator',
+    author='Martin Aspeli',
+    author_email='optilude@gmail.com',
+    url='https://pypi.python.org/pypi/plone.synchronize',
+    license='BSD',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['plone'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+    ],
+    entry_points="""
+    """,
+    )
