adef90642e7b645403d1cf561382289b6fe3e154
Maurits van Rees <maurits@vanrees.org>
2016-02-09 21:11:04 +0100

    Moved rst to txt.

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 from setuptools import setup, find_packages
-import os
 
 version = '1.0.2.dev0'
 
 setup(name='plone.synchronize',
       version=version,
       description="Simple decorators to support synchronized methods",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      long_description=open("README.rst").read() + "\n" +
+                       open("CHANGES.rst").read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Programming Language :: Python",
